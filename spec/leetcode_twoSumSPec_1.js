301fe4e8dc72d2d2cc4f9820a5ff11f7d87d4316
Seven Li <sevenlist0110@gmail.com>
2015-03-08 17:44:41 -0700

    Finished the simple implementation of #1 problem: "Two Sum"

--- spec/leetcode_twoSumSPec_1.js
+++ spec/leetcode_twoSumSPec_1.js
@@ -1,12 +1,14 @@
-describe('coderbyte_firstReversal', function() {
+describe('leetcode_twoSum attempt 1', function() {
 
   beforeEach(function() {
   });
 
-  it('should reverse a string', function() {
-    expect(coderbyte_firstReversal("candy")).to.equal("ydnac");
-    console.log(coderbyte_firstReversal("candy"));
-  	expect(coderbyte_firstReversal("zoo!")).to.equal("!ooz");    
-  	console.log(coderbyte_firstReversal("zoo!"));
+  it('should return the index of 2 number that add up to target', function() {
+    var arr=twoSum_1([1,3,5,6], 8);
+    expect(arr).to.eql([1,2]);
+    console.log(arr);
+    arr=twoSum_1([1,2,4,7],10);
+ 	expect(arr).to.eql([null, null]);
+    console.log(arr);
   });
 });
--- src/leetcode_twoSum_1.js
+++ src/leetcode_twoSum_1.js
@@ -9,6 +9,35 @@ Input: numbers={2, 7, 11, 15}, target=9
 Output: index1=1, index2=2
 */
 
-function twoSum_1(array){
+
+/* first try
+Algorithm:
+	Iterate through each element of the loop 
+	for that element, iterate through eeach element of the loop
+	and find the sum of the 2 numbers
+	if the sum is equal to the target, return the index
+	else, not found, return [null, null]
+
+	Not great but quick implementation
+	O(n^2)
+
+*/
+function twoSum_1(array, target){
+	for (var i=0; i<array.length; i++){
+		for (var j=0; j<array.length; j++){
+			if (j===i){
+				//  if itself 
+				continue; // go to the next j loop
+			}
+			if (array[i]+array[j]===target){
+				// if found such 2 numbers
+				return [i,j];
+			}
+		}
+	}
+
+	// else, if iterate through all the array and didn't find
+	// 2 numbers that adds to target, return null
+	return [null, null];
 	
 }
